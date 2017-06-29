327e488ad0e02733f3ebd7bd1b12e9ffc8cf7b4b
rebeccaseel <rseelgws@gmail.com>
2017-06-28 21:32:21 -0400

    problem 1

--- ConditionalsPracticeProblems/Program.cs
+++ ConditionalsPracticeProblems/Program.cs
@@ -10,6 +10,27 @@ namespace ConditionalsPracticeProblems
     {
         static void Main(string[] args)
         {
+            //set values for problem
+            double firstNumber;
+            double secondNumber;
+            //let user know what to expect with app
+            //have user enter required data
+            Console.WriteLine("Please type two numbers to determine if they are the same.");
+            Console.WriteLine("Enter your first number ");
+            firstNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number ");
+            secondNumber = double.Parse(Console.ReadLine());
+
+            //write formula to calculate answer or solve problem and print answer for user
+            if (firstNumber == secondNumber)
+            {
+                Console.WriteLine("The numbers " + firstNumber + " and " + secondNumber + " are the same number");
+            }
+            else
+            {
+                Console.WriteLine("The numbers " + firstNumber + " and " + secondNumber + " are not equal to each other");
+            }
         }
     }
 }
